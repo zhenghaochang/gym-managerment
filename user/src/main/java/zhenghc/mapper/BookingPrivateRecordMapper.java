b48a2da54f790348b51a1ddc84aee7fd5a5d15b5
zhenghc5 <zhenghc5@asiainfo.com>
2026-04-26 20:08:08 +0800

    4.26 api

--- user/src/main/java/zhenghc/mapper/BookingPrivateRecordMapper.java
+++ user/src/main/java/zhenghc/mapper/BookingPrivateRecordMapper.java
@@ -2,8 +2,13 @@ package zhenghc.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import zhenghc.entity.BookingPrivateRecord;
+import zhenghc.entity.BookingRecord;
+
+import java.util.List;
 
 @Mapper
 public interface BookingPrivateRecordMapper {
     void insert(BookingPrivateRecord param);
+
+    List<BookingPrivateRecord> selectAllByUserId(Long id);
 }
